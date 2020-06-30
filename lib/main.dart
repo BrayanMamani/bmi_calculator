936b8c40ec2d8c6e16237c3b3d3bdbc0703a7e98
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-06-29 22:50:55 -0400

    Separate main widgets in class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bmicalculator/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BMICalculator());
@@ -6,11 +7,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
-        ),
-      ),
+      home: InputPage(),
     );
   }
 }
